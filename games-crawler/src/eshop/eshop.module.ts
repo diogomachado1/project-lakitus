31beeed45ffabf5d744192ef12e7dc595693f00e
Diogo Machado <diogomachado_8@hotmail.com>
2021-09-23 10:13:26 -0300

    deploy

--- games-crawler/src/eshop/eshop.module.ts
+++ games-crawler/src/eshop/eshop.module.ts
@@ -1,8 +1,16 @@
+import * as redisStore from 'cache-manager-redis-store';
 import { CacheModule, Module } from '@nestjs/common';
 import { EshopService } from './eshop.service';
 
 @Module({
-  imports: [CacheModule.register({ ttl: 60 * 60 * 5 })],
+  imports: [
+    CacheModule.register({
+      store: redisStore,
+      host: process.env.REDIS_HOST,
+      port: 6379,
+      ttl: 60 * 60 * 5,
+    }),
+  ],
   providers: [{ useClass: EshopService, provide: 'ESHOP_SERVICE' }],
   exports: ['ESHOP_SERVICE'],
 })
