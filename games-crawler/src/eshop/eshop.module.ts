9fba16125152b3a3fe10e4ec540225d8a954cb75
Diogo Machado <diogomachado_8@hotmail.com>
2021-09-25 20:02:48 -0300

    fix/redis

--- games-crawler/src/eshop/eshop.module.ts
+++ games-crawler/src/eshop/eshop.module.ts
@@ -6,14 +6,14 @@ import { EshopService } from './eshop.service';
   imports: [
     CacheModule.register([
       {
+        store: 'memory',
+        ttl: 60 * 60 * 1,
+      },
+      {
         store: redisStore,
         host: process.env.REDIS_HOST,
         port: 6379,
         ttl: 60 * 60 * 5,
-      },
-      {
-        store: 'memory',
-        ttl: 60 * 60 * 1,
       },
     ]),
   ],
