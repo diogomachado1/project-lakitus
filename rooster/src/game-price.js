65151f7c00e706cf7a3c49def80c373f94283d4e
Diogo Machado <diogomachado_8@hotmail.com>
2021-10-10 13:49:09 -0300

    fix/add-rooster

---
+++ rooster/src/game-price.js
@@ -0,0 +1,9 @@
+'use strict';
+
+const { default: axios } = require('axios');
+
+module.exports.run = async (event, context) => {  
+  const reponse = await axios.post('http://api.leari.xyz/send-games-ids/prices');
+  const time = new Date();
+  console.log(`Your cron function "${context.functionName}" ran at ${time}, status${reponse.status}`);
+};
