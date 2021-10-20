81d8dde3f0c62cd3a55165dcad27ac18fb3f5598
Diogo Machado <diogomachado_8@hotmail.com>
2021-10-20 09:35:09 -0300

    feat/remove-game-service

--- game-service/src/infra/rabbit/rabbit.module.ts
+++
@@ -1,8 +0,0 @@
-import { Module } from '@nestjs/common';
-import { RabbitService } from './rabbit.service';
-
-@Module({
-  providers: [{ useClass: RabbitService, provide: 'RABBIT_SERVICE' }],
-  exports: ['RABBIT_SERVICE'],
-})
-export class RabbitModule {}
--- game-service/src/producer-game-detail/interfaces/NsgGameResponse.ts
+++
@@ -1,7 +0,0 @@
-import { NsgGame } from './NsgGame';
-
-export interface NsgGameResponse {
-  total: number;
-  showing: number;
-  rows: NsgGame[];
-}
