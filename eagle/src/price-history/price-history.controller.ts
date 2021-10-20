c114df1ec978cca1304b46c298311d697e14a60e
Diogo Machado <diogomachado_8@hotmail.com>
2021-10-20 08:47:46 -0300

    feat/merge-price

---
+++ eagle/src/price-history/price-history.controller.ts
@@ -0,0 +1,15 @@
+import { Controller, Param, Get, Query } from '@nestjs/common';
+import { PriceHistoryService } from './price-history.service';
+
+@Controller('price-history')
+export class PriceHistoryController {
+  constructor(private service: PriceHistoryService) {}
+  @Get('/:gameId')
+  async getOnePriceHistory(
+    @Param('gameId') gameId: string,
+    @Query()
+    { country }: { country: string },
+  ) {
+    return this.service.getPriceHistoryByGameIdAndCountry(gameId, country);
+  }
+}
