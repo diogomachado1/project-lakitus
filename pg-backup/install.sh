58ab338270c766c2a2a5529c28d91c85ae3a0198
Diogo Machado <diogomachado_8@hotmail.com>
2021-10-24 10:36:53 -0300

    fix/deploy

--- pg-backup/install.sh
+++ pg-backup/install.sh
@@ -12,6 +12,8 @@ apk add postgresql
 # install s3 tools
 apk add python3 py3-pip
 
+python -m pip install awscli
+
 # install go-cron
 apk add curl
 curl -L --insecure https://github.com/odise/go-cron/releases/download/v0.0.6/go-cron-linux.gz | zcat > /usr/local/bin/go-cron
