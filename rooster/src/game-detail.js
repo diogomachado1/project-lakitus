fbdbaef72a7369506e399761cf19ad59996a482e
Diogo Machado <diogomachado_8@hotmail.com>
2021-12-20 09:11:10 -0300

    feat/add-game-detail-cron

---
+++ rooster/src/game-detail.js
@@ -0,0 +1,9 @@
+'use strict';
+
+const { default: axios } = require('axios');
+
+module.exports.run = async (event, context) => {  
+  const reponse = await axios.post('http://api.leari.xyz/game/send-games-ids');
+  const time = new Date();
+  console.log(`Your cron function "${context.functionName}" ran at ${time}, status${reponse.status}`);
+};
